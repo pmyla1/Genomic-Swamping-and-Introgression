302e6a152076d4ddc9d681007266f9ab1d7d5027
pmyla1 <151543531+pmyla1@users.noreply.github.com>
2024-05-21 15:19:36 +0100

    Update merged_illumina_reads.sh

--- merged_illumina_reads.sh
+++ merged_illumina_reads.sh
@@ -12,14 +12,19 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=pmyla1@exmail.nottingham.ac.uk
 
+############################
+##This script was written by Luke Archer (2024) and can be used to concatenate the R1 and R2 Illumina paired-end reads (.fastq.gz) into a single "interleaved" fastq.gz file. This was performed to increase the speed of alignment production with bwa mem, because the alignments using the paired-end reads separately was extremely slow and memory intensive.
+#####################
+
 ##################
 ##setup 
 source $HOME/.bash_profile
 
 ##cd to 2024.Cochlearia.Illumina.cohort/170524_cutadapt/
-cd /gpfs01/home/pmyla1/2024.Cochlearia.Illumina.cohort/170524_cutadapt
+cd ~/2024.Cochlearia.Illumina.cohort/170524_cutadapt
 
 mkdir 180524_merged_reads/
+
 ###concatenate the R1 and R2 reads for each population into one file for alignment
 cat ./170524_FLEET_2_EKDL240001890-1A_222TKYLT4_L1_1.fq.gz ./170524_FLEET_2_EKDL240001890-1A_222TKYLT4_L1_2.fq.gz > ./180524_merged_FLEET_2_EKDL240001890-1A_222TKYLT4.fq.gz
 
@@ -36,6 +41,7 @@ cat ./170524_SBAY_1_EKDL240001890-1A_222TKYLT4_L1_1.fq.gz ./170524_SBAY_1_EKDL24
 cat ./170524_SPEY_2_EKDL240001890-1A_222TKYLT4_L1_1.fq.gz ./170524_SPEY_2_EKDL240001890-1A_222TKYLT4_L1_2.fq.gz > ./180524_merged_SPEY_2_EKDL240001890-1A_222TKYLT4.fq.gz
 
 cat ./170524_Pen_1_EKDL240001890-1A_222TKYLT4_L1_1.fq.gz ./170524_Pen_1_EKDL240001890-1A_222TKYLT4_L1_2.fq.gz > ./180524_merged_Pen_1_EKDL240001890-1A_222TKYLT4.fq.gz
+###############
 
 echo "DONE!!"
 
