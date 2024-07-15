9c09820a17ee1783e8ff0bd3496796abe7947537
pmyla1 <151543531+pmyla1@users.noreply.github.com>
2024-07-15 12:05:23 +0100

    Update 120724.polyfst.sh

--- FinalScripts/120724.polyfst.sh
+++ FinalScripts/120724.polyfst.sh
@@ -20,30 +20,31 @@ module load gcc-uoneasy/13.2.0
 
 ##cd to appropriate directory
 cd ~/120724.polyfst
-##poly_fst calculating fst
-#~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/pyrenaica.txt -pop2 ~/120724.polyfst/officinalis.txt -mis 0.9 -maf 0.05 -stat fst > ~/120724.polyfst/pyrenaica.officinalis.fst.txt
-
-#~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/pyrenaica.txt -pop2 ~/120724.polyfst/anglica.txt -mis 0.9 -maf 0.05 -stat fst > ~/120724.polyfst/pyrenaica.anglica.fst.txt
-
-#~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/pyrenaica.txt -pop2 ~/120724.polyfst/danica.txt -mis 0.9 -maf 0.05 -stat fst > ~/120724.polyfst/pyrenaica.danica.fst.txt
-
-#~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/officinalis.txt -pop2 ~/120724.polyfst/danica.txt -mis 0.9 -maf 0.05 -stat fst > ~/120724.polyfst/officinalis.danica.fst.txt
-
-#~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/officinalis.txt -pop2 ~/120724.polyfst/anglica.txt -mis 0.9 -maf 0.05 -stat fst > ~/120724.polyfst/officinalis.anglica.fst.txt
-
-#~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/anglica.txt -pop2 ~/120724.polyfst/danica.txt -mis 0.9 -maf 0.05 -stat fst > ~/120724.polyfst/anglica.danica.fst.txt
+##poly_fst calculating fst between pyrenaica and officinalis
+~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/pyrenaica.txt -pop2 ~/120724.polyfst/officinalis.txt -mis 0.9 -maf 0.05 -stat fst > ~/120724.polyfst/pyrenaica.officinalis.fst.txt
+##between pyrenaica and anglica
+~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/pyrenaica.txt -pop2 ~/120724.polyfst/anglica.txt -mis 0.9 -maf 0.05 -stat fst > ~/120724.polyfst/pyrenaica.anglica.fst.txt
+##between pyrenaica and danica
+~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/pyrenaica.txt -pop2 ~/120724.polyfst/danica.txt -mis 0.9 -maf 0.05 -stat fst > ~/120724.polyfst/pyrenaica.danica.fst.txt
+##between danica and officinalis
+~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/officinalis.txt -pop2 ~/120724.polyfst/danica.txt -mis 0.9 -maf 0.05 -stat fst > ~/120724.polyfst/officinalis.danica.fst.txt
+##between anglica and officinalis
+~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/officinalis.txt -pop2 ~/120724.polyfst/anglica.txt -mis 0.9 -maf 0.05 -stat fst > ~/120724.polyfst/officinalis.anglica.fst.txt
+##between anglica and danica
+~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/anglica.txt -pop2 ~/120724.polyfst/danica.txt -mis 0.9 -maf 0.05 -stat fst > ~/120724.polyfst/anglica.danica.fst.txt
 ################
 ##polyfst calculating dxy = nucleotide diversity
+##between pyrenaica and officinalis
 ~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/pyrenaica.txt -pop2 ~/120724.polyfst/officinalis.txt -mis 0.9 -maf 0.05 -stat dxy > ~/pyrenaica.officinalis.dxy
-
+##between pyrenaica and anglica
 ~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/pyrenaica.txt -pop2 ~/120724.polyfst/anglica.txt -mis 0.9 -maf 0.05 -stat dxy > ~/pyrenaica.anglica.dxy
-
+##between pyrenaica and danica
 ~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/pyrenaica.txt -pop2 ~/120724.polyfst/danica.txt -mis 0.9 -maf 0.05 -stat dxy > ~/pyrenaica.danic.dxy
-
+##between danica and officinalis
 ~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/danica.txt -pop2 ~/120724.polyfst/officinalis.txt -mis 0.9 -maf 0.05 -stat dxy > ~/danica.officinalis.dxy
-
+##between anglica and officinalis
 ~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/anglica.txt -pop2 ~/120724.polyfst/officinalis.txt -mis 0.9 -maf 0.05 -stat dxy > ~/anglica.officinalis.dxy
-
+##between anglica and danica
 ~/scripts/poly_fst -vcf ~/120624_LD.Pruned.Ionops.allUKsamples.copy.vcf -pop1 ~/120724.polyfst/anglica.txt -pop2 ~/120724.polyfst/danica.txt -mis 0.9 -maf 0.05 -stat dxy > ~/anglica.danica.dxy
 ##############
 
