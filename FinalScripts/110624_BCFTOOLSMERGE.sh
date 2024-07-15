96147bb7110f8c763639d23411cc3dbebc3e78c2
pmyla1 <151543531+pmyla1@users.noreply.github.com>
2024-07-15 11:57:31 +0100

    Update 110624BCFTOOLSMERGE.sh

--- FinalScripts/110624_BCFTOOLSMERGE.sh
+++ FinalScripts/110624_BCFTOOLSMERGE.sh
@@ -29,10 +29,12 @@ OUTMERGED=~/300524_HaplotypeCaller_output/090624_combined_genotyped/100624_filte
 VCF1OLD=~/300524_HaplotypeCaller_output/090624_combined_genotyped/100624_filtered.best/110624_Ion.dan.filtered.F4.g.vcf.gz
 VCF1NEW=~/300524_HaplotypeCaller_output/090624_combined_genotyped/100624_filtered.best/110624_reheadered.Ion.dan.filtered.F4.g.vcf.gz
 #############
-##use bcftools view to see the header 
-#bcftools view -h $VCF1OLD > ./header1.txt
+##use bcftools view to see the header, store in a new txt file called header{n}.txt so you can manually alter using nano
+bcftools view -h $VCF1OLD > ./header1.txt
 
-#bcftools view -h $VCF2OLD > ./header2.txt
+bcftools view -h $VCF2OLD > ./header2.txt
+###########
+##AT THIS POINT THE ORIGINAL SCRIPT ENDED HERE SO YOU CAN NAVIGATE TO THE DIRECTORY CONTAINING THE HEADER1.TXT AND HEADER2.TXT FILES AND MANUALLY ALTER BEFORE EXECUTING THE REST OF THE SCRIPT
 #############
 ##manually altered the header1.txt and header2.txt PL fields to Number=.
 bcftools reheader -h header1.txt $VCF1OLD > $VCF1NEW
