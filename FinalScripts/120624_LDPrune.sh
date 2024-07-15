27f5382874bff4f4c282485d74ffd17526e68255
pmyla1 <151543531+pmyla1@users.noreply.github.com>
2024-07-15 12:02:42 +0100

    Update FinalSelectVarsLDPrune.sh

--- FinalScripts/120624_LDPrune.sh
+++ FinalScripts/120624_LDPrune.sh
@@ -20,38 +20,38 @@ module load gatk-uoneasy/4.4.0.0-GCCcore-12.3.0-Java-17
 
 cd ~/300524_HaplotypeCaller_output/090624_combined_genotyped/110624_filtered.best/
 #####
-#VCF=~/300524_HaplotypeCaller_output/090624_combined_genotyped/100624_filtered.best/110624_Merged.F4_133.ann.Ion.dan.filtered.F4.vcf.gz
+VCF=~/300524_HaplotypeCaller_output/090624_combined_genotyped/100624_filtered.best/110624_Merged.F4_133.ann.Ion.dan.filtered.F4.vcf.gz
 OUTVCF=~/300524_HaplotypeCaller_output/090624_combined_genotyped/110624_filtered.best/120624_Ionops.allUKsamples.F4.vcf.gz
 ##index the 110624_reheadered.F4_133.ann.vcf.gz file with gatk IndexFeatureFile
-#gatk IndexFeatureFile -I $VCF
+gatk IndexFeatureFile -I $VCF
 
 ######
 ###use GATK SelectVariants to select all Ionopsidium, UK diploids, tetraploids, C. danica, and putative C. anglica
-#gatk SelectVariants -V $VCF \
-# --select-type-to-include SNP \
-# --restrict-alleles-to BIALLELIC \
-# -sn AAH_1 -sn AAH_2 -sn AAH_3 -sn AAH_4 \
-# -sn ALO_006 -sn ALO_007 -sn ALO_013 -sn ALO_017 \
-# -sn BNK_21 -sn BRE_1 -sn CHA_1 -sn CHA_2 -sn CUM_1 \
-# -sn DAR_1 -sn DAR_3 -sn ELI_001 -sn ELI_002 -sn ELI_003 -sn ELI_004 \
-# -sn ERS_1 -sn ERS_2 -sn ERS_3 -sn ERS_4 -sn FOR_1 -sn FRE_013 \
-# -sn FTW_1 -sn FTW_2 -sn FTW_3 -sn FTW_5 -sn GEO_2 -sn GEO_6 \
-# -sn Ime -sn Iac -sn Iab_1 -sn Iab_2 \
-# -sn JON_001 -sn JOR_1 -sn JOR_12 -sn JOR_13 -sn JOR_3 \
-# -sn LAB_004 -sn LAB_1 -sn LAB_2 -sn LAB_300 -sn LAB_4 -sn LAB_400 -sn LAB_5 -sn LAB_500 \
-# -sn LAL_1 -sn LAL_2 -sn LAL_3 -sn LAL_4 -sn LNL_001 -sn LNL_002 -sn LNL_003 -sn LNL_008 \
-# -sn LOS_1 -sn LOS_6 -sn LOS_7 -sn NEI_1 -sn NEI_3 -sn NEI_8 -sn NEI_9 \
-# -sn NEN_001 -sn NEN_003 -sn NEN_200 -sn NEN_300 -sn NEN_4 -sn NEN_5 -sn NEN_6 \
-# -sn ODN_10 -sn ODN_2 -sn ODN_4 -sn ODN_5 -sn ODN_6 -sn ODN_7 -sn ODN_9 \
-# -sn PAR_2 -sn Pen_1 -sn NOT -sn LWS \
-# -sn ROT_004 -sn ROT_006 -sn ROT_007 -sn ROT_013 -sn RYE_1 -sn SPEY_2 -sn SCO_1 \
-# -sn SCU_1 -sn SCU_14 -sn SCU_15 -sn SCU_16 -sn SCU_19 \
-# -sn SKF_002 -sn SKF_003 -sn SKF_005 -sn SKF_009 \
-# -sn SKN_001 -sn SKN_002 -sn SKN_005 -sn SKN_008 \
-# -sn SPU_006 -sn SPU_008 -sn SPU_009 -sn SPU_010 \
-# -sn TET_002 -sn TET_004 -sn TET_006 -sn TET_008 \
-# -O $OUTVCF \
-# --allow-nonoverlapping-command-line-samples
+gatk SelectVariants -V $VCF \
+ --select-type-to-include SNP \
+ --restrict-alleles-to BIALLELIC \
+ -sn AAH_1 -sn AAH_2 -sn AAH_3 -sn AAH_4 \
+ -sn ALO_006 -sn ALO_007 -sn ALO_013 -sn ALO_017 \
+ -sn BNK_21 -sn BRE_1 -sn CHA_1 -sn CHA_2 -sn CUM_1 \
+ -sn DAR_1 -sn DAR_3 -sn ELI_001 -sn ELI_002 -sn ELI_003 -sn ELI_004 \
+ -sn ERS_1 -sn ERS_2 -sn ERS_3 -sn ERS_4 -sn FOR_1 -sn FRE_013 \
+ -sn FTW_1 -sn FTW_2 -sn FTW_3 -sn FTW_5 -sn GEO_2 -sn GEO_6 \
+ -sn Ime -sn Iac -sn Iab_1 -sn Iab_2 \
+ -sn JON_001 -sn JOR_1 -sn JOR_12 -sn JOR_13 -sn JOR_3 \
+ -sn LAB_004 -sn LAB_1 -sn LAB_2 -sn LAB_300 -sn LAB_4 -sn LAB_400 -sn LAB_5 -sn LAB_500 \
+ -sn LAL_1 -sn LAL_2 -sn LAL_3 -sn LAL_4 -sn LNL_001 -sn LNL_002 -sn LNL_003 -sn LNL_008 \
+ -sn LOS_1 -sn LOS_6 -sn LOS_7 -sn NEI_1 -sn NEI_3 -sn NEI_8 -sn NEI_9 \
+ -sn NEN_001 -sn NEN_003 -sn NEN_200 -sn NEN_300 -sn NEN_4 -sn NEN_5 -sn NEN_6 \
+ -sn ODN_10 -sn ODN_2 -sn ODN_4 -sn ODN_5 -sn ODN_6 -sn ODN_7 -sn ODN_9 \
+ -sn PAR_2 -sn Pen_1 -sn NOT -sn LWS \
+ -sn ROT_004 -sn ROT_006 -sn ROT_007 -sn ROT_013 -sn RYE_1 -sn SPEY_2 -sn SCO_1 \
+ -sn SCU_1 -sn SCU_14 -sn SCU_15 -sn SCU_16 -sn SCU_19 \
+ -sn SKF_002 -sn SKF_003 -sn SKF_005 -sn SKF_009 \
+ -sn SKN_001 -sn SKN_002 -sn SKN_005 -sn SKN_008 \
+ -sn SPU_006 -sn SPU_008 -sn SPU_009 -sn SPU_010 \
+ -sn TET_002 -sn TET_004 -sn TET_006 -sn TET_008 \
+ -O $OUTVCF \
+ --allow-nonoverlapping-command-line-samples
 ##########
 
 ##unload GATK module
